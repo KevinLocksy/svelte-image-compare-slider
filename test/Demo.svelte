ca34b5d8edc5cdc2a1e3814bf84609ffe89caced
Kevin Locksy <nhan.kev@gmail.com>
2024-01-20 20:37:34 +0100

    Start demo development

--- test/Demo.svelte
+++ test/Demo.svelte
@@ -1,21 +1,53 @@
+<svelte:head>
+  <title>Demonstration Component Image Compare Slider</title>
+</svelte:head>
+
 <script>
-import {ImageCompareSlider} from 'svelte-image-compare-slider'
-//import ImageCompareSlider from svelte-image-compare-slider
-  const height = "50px"; //does not work with %
-  const imgLeft_src = "";
+//import {ImageCompareSlider} from '../src/ImageCompareSlider'
+import ImageCompareSlider from '../src/ImageCompareSlider.svelte'
+  const height = "250px"; //does not work with %
+  const imgLeft_src = "https://www.w3schools.com/howto/img_forest.jpg";
   const imgLeft_alt = "left";
-  const imgRight_src = "";
+  const imgRight_src = "https://www.w3schools.com/howto/img_snow.jpg";
   const imgRight_alt = "right";
 </script>
 
-<!-- displays the image comparison -->
-<div>
-test
-<!--
-<ImageCompareSlider height={height}
-                    left_src={imgLeft_src}
-                    left_alt={imgLeft_alt}
-                    right_src={imgRight_src}
-                    right_alt={imgRight_alt} />
-                    -->
+<div id="container">
+  <div>
+    <h1>Image Comparison Slider </h1>
+  </div>
+  <div id="test">
+  <ImageCompareSlider height={height}
+                    leftSrc={imgLeft_src}
+                    leftAlt={imgLeft_alt}
+                    rightSrc={imgRight_src}
+                    rightAlt={imgRight_alt} />
+  </div>
+  <div id='footer'>
+    Developed by Kevin Locksy
+  </div>
 </div>
+
+<style>
+  #test{
+    position: relative;
+    display: flex;
+  }
+  #container{
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+/*    text-align:center;*/
+  }
+  #container > *{
+    justify-content: center;
+  }
+
+  #footer{
+    display: flex;
+    position: fixed;
+    bottom: 0%;
+    width:100%;
+    background-color: blue;
+  }
+</style>
